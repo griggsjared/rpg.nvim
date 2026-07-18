4dca4137caa8919e0b51bae1d4fb9da7ae750189
Jared Griggs <griggsjared@gmail.com>
2026-07-17 22:36:21 -0600

    fix(render-markdown): use default heading highlights

--- lua/rpg/theme/plugins/markdown.lua
+++ lua/rpg/theme/plugins/markdown.lua
@@ -6,16 +6,32 @@ local M = {}
 ---@param hp table Helper functions/modules.
 ---@return table<string, table> highlights Render Markdown highlight group definitions.
 function M.get(c, hp)
-  return {
-    RenderMarkdownHeader1 = { fg = c.base.green },
-    RenderMarkdownHeader2 = { fg = c.base.magenta },
-    RenderMarkdownHeader3 = { fg = c.base.yellow },
-    RenderMarkdownHeader4 = { fg = c.base.cyan },
-    RenderMarkdownHeader5 = { fg = c.base.red },
-    RenderMarkdownHeader6 = { fg = c.base.blue },
-    RenderMarkdownCode = { bg = c.base.background },
-    RenderMarkdownCodeInline = { bg = c.base.background },
-  }
+	return {
+		RenderMarkdownH1 = { fg = c.base.green },
+		RenderMarkdownH2 = { fg = c.base.magenta },
+		RenderMarkdownH3 = { fg = c.base.yellow },
+		RenderMarkdownH4 = { fg = c.base.cyan },
+		RenderMarkdownH5 = { fg = c.base.red },
+		RenderMarkdownH6 = { fg = c.base.blue },
+		RenderMarkdownH1Bg = { bg = c.base.background },
+		RenderMarkdownH2Bg = { bg = c.base.background },
+		RenderMarkdownH3Bg = { bg = c.base.background },
+		RenderMarkdownH4Bg = { bg = c.base.background },
+		RenderMarkdownH5Bg = { bg = c.base.background },
+		RenderMarkdownH6Bg = { bg = c.base.background },
+		RenderMarkdownCode = { bg = c.base.background },
+		RenderMarkdownCodeInline = { bg = c.base.background },
+		RenderMarkdownCodeInfo = { fg = c.base.dimmed2 },
+		RenderMarkdownCodeBorder = { fg = c.base.dimmed4 },
+		RenderMarkdownQuote = { fg = c.base.foreground },
+		RenderMarkdownBullet = { fg = c.base.dimmed2 },
+		RenderMarkdownDash = { fg = c.base.dimmed2 },
+		RenderMarkdownTableHead = { fg = c.base.yellow },
+		RenderMarkdownTableRow = { fg = c.base.foreground },
+		RenderMarkdownLink = { fg = c.base.cyan },
+		RenderMarkdownSign = { fg = c.base.dimmed3 },
+		RenderMarkdownMath = { fg = c.base.cyan },
+	}
 end
 
 return M
